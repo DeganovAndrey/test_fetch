ac57663d4becd3ea302ad51e98fcf9025dfbda27
DeganovAndrey <Deganovdyoosha@yandex.ru>
2024-12-18 16:13:56 +0300

    test_push_all

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,21 +1,34 @@
 import { useEffect, useState } from "react";
 import Post from "./Post";
 
+const API_URL = "http://jsonplaceholder.typicode.com/posts";
+
 const Posts = () => {
   const [posts, setPosts] = useState([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
-    fetch("http://jsonplaceholder.typicode.com/posts")
+    fetch(API_URL)
       .then((res) => res.json())
-      .then((posts) => {
-        console.log(posts, setPosts(posts));
-      })
-      .catch((error) => console.log(error.message));
+      .then((posts) => setPosts(posts))
+      .catch((error) => setError(error.message))
+      .finally(() => setIsLoading(false));
   }, []);
+
+  if (error) {
+    return <h1>Error: {error}</h1>;
+  }
+
   return (
-    <div>
-      <Post />
-      <Post />
-    </div>
+    <>
+      <h1>Posts</h1>
+      <hr />
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        posts.map((post) => <Post key={post.id} {...post} />)
+      )}
+    </>
   );
 };
 
